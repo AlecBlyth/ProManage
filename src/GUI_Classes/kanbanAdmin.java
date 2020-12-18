da36149bff4738b43ca8a320f0db89326c342cef
Alec <alecblyth@outlook.com>
2020-12-18 11:03:24 +0000

    Change Log: 0.0.8
    Changed Team Chat to Chat
    Replaced Kanban pane to flowpane to allow for dragging
    
    Added limited JSON functionality

--- src/GUI_Classes/kanbanAdmin.java
+++ src/GUI_Classes/kanbanAdmin.java
@@ -1,18 +1,55 @@
 package GUI_Classes;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class kanbanAdmin {
 
+    public Label lblBacklog, lblTodo, lblProgress, lblComplete, lblBlocked;
     private double xOffset = 0;
     private double yOffset = 0;
+    @FXML private FlowPane paneOne, paneTwo, paneThree, paneFour, paneFive;
+
+    public void initialize(){
+
+        JSONParser parser = new JSONParser();
+        try{
+            Object obj = parser.parse(new FileReader("src/Datafiles/logs/ProjectFile.json"));
+            JSONObject jsonObject = (JSONObject) obj;
+            JSONArray testList = (JSONArray) jsonObject.get("projectLabels");
+
+            lblBacklog.setText(testList.get(0).toString());
+            lblTodo.setText(testList.get(1).toString());
+            lblProgress.setText(testList.get(2).toString());
+            lblComplete.setText(testList.get(3).toString());
+            lblBlocked.setText(testList.get(4).toString());
+
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 
 
     public void kanban(ActionEvent actionEvent) {
