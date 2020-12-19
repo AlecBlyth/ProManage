7251f6ca5c51ccfa19e6f140fb43127c188e8a0f
Alec <alecblyth@outlook.com>
2020-12-19 15:21:27 +0000

    Change Log: 0.2.4
    Added Template Classes for FXML files

---
+++ src/GUI_Classes/tasks.java
@@ -0,0 +1,171 @@
+package GUI_Classes;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.animation.Animation;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+public class tasks {
+    //FXML Components
+    public Label lblDate;
+    public Label lblTime;
+    public ImageView memberIcon;
+    public ImageView reqIcon;
+    public JFXButton btnMembers;
+    public JFXButton btnRequests;
+
+    //Variables
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+    //Passed Variables
+    public String currentUser;
+
+    //SYSTEM METHODS
+    public static String getFormattedDate(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        int day = cal.get(Calendar.DATE);
+
+        if (!((day > 10) && (day < 19))) //Allows for th st nd suffixes
+            switch (day % 10) {
+                case 1:
+                    return new SimpleDateFormat("d'st' MMMM yyyy").format(date);
+                case 2:
+                    return new SimpleDateFormat("d'nd' MMMM yyyy").format(date);
+                case 3:
+                    return new SimpleDateFormat("d'rd' MMMM yyyy").format(date);
+                default:
+                    return new SimpleDateFormat("d'th' MMMM yyyy").format(date);
+            }
+        return new SimpleDateFormat("d'th' MMMM yyyy").format(date);
+    } //Date formatter for date label
+
+    public void initialize(String userType) {
+        initTime();
+        currentUser = userType; //Sets currentUser to userType
+
+        if ("ADMIN".equals(userType)) {
+            btnMembers.setVisible(true);
+            btnRequests.setVisible(true);
+            memberIcon.setVisible(true);
+            reqIcon.setVisible(true);
+            btnMembers.setDisable(false);
+            btnRequests.setDisable(false);
+        } else {
+            btnMembers.setVisible(false);
+            btnRequests.setVisible(false);
+            memberIcon.setVisible(false);
+            reqIcon.setVisible(false);
+            btnMembers.setDisable(true);
+            btnRequests.setDisable(true);
+        }
+    } //Initialise controller
+
+    public void initTime() {
+        Calendar cal = Calendar.getInstance();
+        lblDate.setText(getFormattedDate(cal.getTime()) + "  |  "); //Gets date and changes label to date
+        DateTimeFormatter SHORT_TIME_FORMATTER = DateTimeFormatter.ofPattern("hh:mm");
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0),
+                event -> lblTime.setText(LocalTime.now().format(SHORT_TIME_FORMATTER))),
+                new KeyFrame(Duration.seconds(1)));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play(); //Updates the clock
+    } //Initialise time
+
+    //ADMIN AND USER FEATURES
+    public void kanban(ActionEvent kanban) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXMLs/kanban.fxml"));
+        AnchorPane root = loader.load();
+        GUI_Classes.kanban kanbanScene = loader.getController();
+        kanbanScene.initialize(currentUser);
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        Scene menuViewScene = new Scene(root);
+        Stage window = (Stage) ((Node) kanban.getSource()).getScene().getWindow();
+        root.setOnMouseDragged(event -> {
+            window.setX((event.getScreenX() - xOffset));
+            window.setY((event.getScreenY() - yOffset));
+        });
+        window.setScene(menuViewScene);
+        window.show();
+    }
+
+    public void tasks(ActionEvent tasks) {
+        //DO NOTHING
+    }
+
+    public void chat(ActionEvent chat) {
+
+    }
+
+    public void profile(ActionEvent profile) {
+    }
+
+    //ADMIN FEATURES
+    public void members(ActionEvent members) {
+    }
+
+    public void requests(ActionEvent requests) {
+    }
+
+    //NAVIGATION
+    public void exit(ActionEvent exit) { //Exit functionality
+        System.exit(0);
+    }
+
+    public void logOut(ActionEvent logout) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXMLs/login.fxml"));
+        AnchorPane root = loader.load();
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        Scene menuViewScene = new Scene(root);
+        Stage window = (Stage) ((Node) logout.getSource()).getScene().getWindow();
+        root.setOnMouseDragged(event -> {
+            window.setX((event.getScreenX() - xOffset));
+            window.setY((event.getScreenY() - yOffset));
+        });
+        window.setScene(menuViewScene);
+        window.show();
+    }
+
+    public void menu(MouseEvent mouseEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXMLs/menu.fxml"));
+        AnchorPane root = loader.load();
+        menu menu = loader.getController();
+        menu.initialize(currentUser);
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        Scene menuViewScene = new Scene(root);
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        root.setOnMouseDragged(event -> {
+            window.setX((event.getScreenX() - xOffset));
+            window.setY((event.getScreenY() - yOffset));
+        });
+        window.setScene(menuViewScene);
+        window.show();
+    }
+}
