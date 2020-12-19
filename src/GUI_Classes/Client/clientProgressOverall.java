b231b2f7373b6469cb893ce0200bad543f619728
Alec <alecblyth@outlook.com>
2020-12-19 14:14:38 +0000

    Change Log: 0.2.1
    Merged and removed fxmls/classes which similar operations
    
    cleaned up code
    added more comments
    added new time method

--- src/GUI_Classes/Client/clientProgressOverall.java
+++ src/GUI_Classes/Client/clientProgressOverall.java
@@ -1,6 +1,5 @@
 package GUI_Classes.Client;
 
-import com.jfoenix.controls.JFXButton;
 import eu.hansolo.medusa.Gauge;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
@@ -10,40 +9,40 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
-import javax.swing.*;
 import java.io.IOException;
 import java.sql.*;
 import java.text.SimpleDateFormat;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
 public class clientProgressOverall {
 
+    //FXML Components
     public Gauge gaugeProgress;
+    public Label lblDate, lblTime;
+
+    //Variables
     private double xOffset = 0;
     private double yOffset = 0;
+    private int sum = 0;
+    private int completeTotal;
 
-    public Label lblDate;
-    public Label lblTime;
-
-    private int minute;
-    private int hour;
-
-    int sum = 0;
-    int compeleteTotal;
-
-    public static String getFormattedDate(Date date){
+    //SYSTEM METHODS
+    public static String getFormattedDate(Date date) {
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
-        int day=cal.get(Calendar.DATE);
+        int day = cal.get(Calendar.DATE);
 
-        if(!((day>10) && (day<19))) //Allows for th st nd suffixes
+        if (!((day > 10) && (day < 19)))
             switch (day % 10) {
                 case 1:
                     return new SimpleDateFormat("d'st' MMMM yyyy").format(date);
@@ -55,74 +54,80 @@ public class clientProgressOverall {
                     return new SimpleDateFormat("d'th' MMMM yyyy").format(date);
             }
         return new SimpleDateFormat("d'th' MMMM yyyy    ").format(date);
-    } //Date formatter for date label Label lblTips;
+    }
 
-    public void initialize(){
+    public void initialize() {
+
+        initTime();
 
         ArrayList<Integer> TaskProgress = new ArrayList<>();
 
-        Timeline clock = new Timeline(new KeyFrame(Duration.ZERO, e -> { //Updates clock every second and changes label according to time
-            Calendar cal = Calendar.getInstance();
-            minute = cal.get(Calendar.MINUTE);
-            hour = cal.get(Calendar.HOUR);
-            String curTime = String.format("%02d:%02d", hour, minute);
-            lblTime.setText(curTime);
-            lblDate.setText(getFormattedDate(cal.getTime())); //Gets date and changes label to date
-        }),
-                new KeyFrame(Duration.seconds(1))
-        );
-
-        clock.setCycleCount(Animation.INDEFINITE);
-        clock.play();
-
-        gaugeProgress.barColorProperty().setValue(Color.rgb(45,121,255)); //Changes colour of value bar
-        gaugeProgress.valueColorProperty().setValue(Color.WHITE); //Changes font colour
+        gaugeProgress.barColorProperty().setValue(Color.rgb(45, 121, 255)); //Changes colour of gauge bar
+        gaugeProgress.valueColorProperty().setValue(Color.WHITE); //Changes font colour of gauge
 
         try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/companyusers", "root", "admin"); //Connect to mySQL dummy database |NOTE This is prone to SQL Injection
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/companyusers", "root", "admin"); //Connect to mySQL server
             Statement statement = connection.createStatement();
-            String queryString = "SELECT taskprogress FROM tasks"; //get tasks from database
+            String queryString = "SELECT taskprogress FROM tasks";
             ResultSet resultSet = statement.executeQuery(queryString);
 
-            while (resultSet.next()) { //Checks for username and password
+            while (resultSet.next()) {
                 int taskprog = resultSet.getInt("taskprogress");
                 TaskProgress.add(taskprog);
-                compeleteTotal = compeleteTotal+1;
+                completeTotal = completeTotal + 1;
             }
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
 
-        for(Integer tempSum : TaskProgress){ //Calculates average percentage
-            sum+= tempSum;
+        for (Integer tempSum : TaskProgress) { //Calculates average percentage
+            sum += tempSum;
         }
-        compeleteTotal = compeleteTotal * 100;
+        completeTotal = completeTotal * 100;
         double currentProgress = sum;
-        double totalPercentage = currentProgress / compeleteTotal * 100;
-        gaugeProgress.setValue(totalPercentage);
+        double totalPercentage = currentProgress / completeTotal * 100;
+        gaugeProgress.setValue(totalPercentage); //Sets gauge value to total percentage
 
     }
 
-    public void progress(ActionEvent actionEvent) {
+    public void initTime() {
+        Calendar cal = Calendar.getInstance();
+        lblDate.setText(getFormattedDate(cal.getTime()) + "  |  ");
+        DateTimeFormatter SHORT_TIME_FORMATTER = DateTimeFormatter.ofPattern("hh:mm");
+        Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(0),
+                event -> lblTime.setText(LocalTime.now().format(SHORT_TIME_FORMATTER))),
+                new KeyFrame(Duration.seconds(1)));
+        timeline.setCycleCount(Animation.INDEFINITE);
+        timeline.play();
+    }
+
+    //CLIENT FEATURES
+    public void progress(ActionEvent progress) {
         //Do nothing since already on stage.
     }
 
-    public void chat(ActionEvent actionEvent) {
+    public void chat(ActionEvent chat) {
     }
 
-    public void profile(ActionEvent actionEvent) {
+    public void request(ActionEvent request) {
     }
 
-    public void request(ActionEvent actionEvent) {
+    public void btnViewmore(ActionEvent viewMore) {
     }
 
-    public void btnViewmore(ActionEvent actionEvent) {
+    //USER FEATURES
+    public void profile(ActionEvent profile) {
+    }
+
+    //NAVIGATION
+    public void exit(ActionEvent exit) {
+        System.exit(0);
     }
 
     public void logOut(ActionEvent logout) throws IOException {
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXMLs/login.fxml")); //Display admin menu
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXMLs/login.fxml"));
         AnchorPane root = loader.load();
-        root.setOnMousePressed(event -> { //Allow to move app around
+        root.setOnMousePressed(event -> {
             xOffset = event.getSceneX();
             yOffset = event.getSceneY();
         });
@@ -132,11 +137,24 @@ public class clientProgressOverall {
             window.setX((event.getScreenX() - xOffset));
             window.setY((event.getScreenY() - yOffset));
         });
-        window.setScene(menuViewScene); //Show new scene
+        window.setScene(menuViewScene);
         window.show();
     }
 
-    public void exit(ActionEvent actionEvent) {
-        System.exit(0);
+    public void menu(MouseEvent mouseEvent) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/FXMLs/Client/clientMenu.fxml"));
+        AnchorPane root = loader.load();
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        Scene menuViewScene = new Scene(root);
+        Stage window = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        root.setOnMouseDragged(event -> {
+            window.setX((event.getScreenX() - xOffset));
+            window.setY((event.getScreenY() - yOffset));
+        });
+        window.setScene(menuViewScene);
+        window.show();
     }
 }
